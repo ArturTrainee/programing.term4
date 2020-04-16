4c4c305d3ed5eaf8e9d5b6852d0ff5554cee7f07
Artur Stecenko <ar2rstecenk0@gmail.com>
2020-04-16 10:56:29 +0300

    Refactored Lab 2

--- Lab2.Task1/Program.cs
+++ Lab2.Task1/Program.cs
@@ -4,8 +4,20 @@ using static System.Console;
 
 namespace Lab2.Task1
 {
-    static class Program
+    public static class Program
     {
+        public static readonly IDictionary<string, Func<int, double>> calcMethods = new Dictionary<string, Func<int, double>>
+            {
+                { "0", CalculateSqrt },
+                { "1", CalculateQube },
+                { "2", AddThreeAndHalf }
+            };
+
+        private static double AddThreeAndHalf(int value) => value + 3.5;
+
+        private static double CalculateQube(int value) => Math.Pow(value, 3);
+
+        private static double CalculateSqrt(int value) => Math.Sqrt(Math.Abs(value));
 
         private static void Main(string[] args)
         {
@@ -13,13 +25,6 @@ namespace Lab2.Task1
             WriteLine("0 x\n1 x\n2 x");
             WriteLine("Options: \n0 -- sqrt(abs(x);\n1 -- x^3;\n2 -- x + 3,5];\nx - some integer\n");
             WriteLine("Enter any other symbol to exit.\n");
-
-            IDictionary<string, Func<int, double>> calcMethods = new Dictionary<string, Func<int, double>>
-            {
-                { "0", CalculateSqrt },
-                { "1", CalculateQube },
-                { "2", AddThreeAndHalf }
-            };
 
             while (true)
             {
@@ -39,11 +44,5 @@ namespace Lab2.Task1
                 }
             }
         }
-
-        private static double CalculateSqrt(int value) => Math.Sqrt(Math.Abs(value));
-
-        private static double CalculateQube(int value) => Math.Pow(value, 3);
-
-        private static double AddThreeAndHalf(int value) => value + 3.5;
     }
 }
