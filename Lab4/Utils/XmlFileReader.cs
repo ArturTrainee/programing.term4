12d72f33f24929ce8408bd6c13e37a0d6d357376
Artur <34406335+ArturTrainee@users.noreply.github.com>
2020-05-27 17:51:00 +0300

    Added xml entities reader/writer

---
+++ Lab4/Utils/XmlFileReader.cs
@@ -0,0 +1,43 @@
+﻿using Lab4.Entities;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Lab4.Utils
+{
+    class XmlEntitiesReader<T> where T : IXmlSerializable, new()
+    {
+        public static IList<T> ReadEntitiesFrom(string path, string searchNodeName, string stopNodeName) 
+        {
+            var readingResult = new List<T>();
+            try
+            {
+                using (var fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    using (var reader = XmlReader.Create(fs))
+                    {
+                        reader.MoveToContent();
+
+                        while (reader.Read())
+                        {
+                            if (reader.Name.Equals(searchNodeName))
+                            {
+                                T repertoire = new T();
+                                repertoire.ReadXml(reader);
+                                readingResult.Add(repertoire);
+                            }
+                            if (reader.Name.Equals(stopNodeName)) break;
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(Messages.FILE_READING_EXCEPTION + "\nДеталі помилки: " + ex.Message);
+            }
+            return readingResult;
+        }
+    }
+}
---
+++ Lab4/Utils/XmlFileWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Lab4.Utils
+{
+    class XmlFileWriter<T>
+    {
+        public static void WriteEntitiesTo(string path, IList<T> entities)
+        {
+            using (var fs = new FileStream(path, FileMode.CreateNew, FileAccess.Write))
+            {
+                var writeSettings = new XmlWriterSettings();
+                writeSettings.Indent = true;
+                writeSettings.ConformanceLevel = ConformanceLevel.Auto;
+                using (var writer = XmlWriter.Create(fs, writeSettings))
+                {
+                    var serializer = new XmlSerializer(typeof(List<T>));
+                    try
+                    {
+                        serializer.Serialize(writer, entities);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
