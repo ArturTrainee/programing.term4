effee38fb2917e918a6c1c6b3c80bf78c91551bb
Artur <34406335+ArturTrainee@users.noreply.github.com>
2020-05-28 16:20:39 +0300

    Removed labs 1, 3, 4

--- Lab4/Utils/Messages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lab4.Entities
-{
-    class Messages
-    {
-        public const string CHANGES_CANCELED = "Ви самі вибрали _Н_Е_ зберігати";
-        public const string FILE_READING_EXCEPTION = "Не вдалося знайти файл із раніше збереженими даними,\nпочинаємо <<з чистого листа>>.";
-    }
-}
